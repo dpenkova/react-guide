faab14bd04ae5f15b633b80cd1e71acee691f9d1
Daniela Penkova <dpenkova@cc.local>
2018-06-29 14:02:25 +0300

    handlers

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
-import React from 'react';  
+import React from 'react';
 
 const person = (props) => {
-    return(
+    return (
         <div>
-            <p>I'm {props.name} and I'm {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
     );
